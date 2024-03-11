9f4b2609c432d2f88f4b712a54864a8a0c155606
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2024-03-11 19:19:51 +0100

    Prettier

--- src/main/java/oslomet/testing/API/AdminKundeController.java
+++ src/main/java/oslomet/testing/API/AdminKundeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/adminKunde")
 public class AdminKundeController {
-    
+
     @Autowired
     AdminRepository repository;
 
@@ -21,7 +21,7 @@ public class AdminKundeController {
     @GetMapping("/hentAlle")
     public List<Kunde> hentAlle() {
         String personnummer = sjekk.loggetInn();
-        if (personnummer!=null) {
+        if (personnummer != null) {
             return repository.hentAlleKunder();
         }
         return null;
@@ -30,7 +30,7 @@ public class AdminKundeController {
     @PostMapping("/lagre")
     public String lagreKunde(@RequestBody Kunde innKunde) {
         String personnummer = sjekk.loggetInn();
-        if (personnummer!=null) {
+        if (personnummer != null) {
             return repository.registrerKunde(innKunde);
         }
         return "Ikke logget inn";
@@ -39,7 +39,7 @@ public class AdminKundeController {
     @PostMapping("/endre")
     public String endre(@RequestBody Kunde innKunde) {
         String personnummer = sjekk.loggetInn();
-        if (personnummer!=null) {
+        if (personnummer != null) {
             return repository.endreKundeInfo(innKunde);
         }
         return "Ikke logget inn";
@@ -48,12 +48,9 @@ public class AdminKundeController {
     @GetMapping("/slett")
     public String slett(String personnummer) {
         String p = sjekk.loggetInn();
-        if (p !=null) {
+        if (p != null) {
             return repository.slettKunde(personnummer);
         }
         return "Ikke logget inn";
     }
 }
-
-
-
